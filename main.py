12abca5d4addde08f060366547861fca7e77e477
ambrosemcduffy <ambrosemcduffy@gmail.com>
2021-02-01 20:19:01 +1100

    adding code

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "2232134JH4RBFBW4123NDK89"
 from routes import *
 
-#if __name__ == "__main__":
-#  app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
